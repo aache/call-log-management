9b1283a8f36a45fd201a847a27ef456404daadce
aaqibhamdule@yahoo.com <aaqibhamdule@yahoo.com>
2019-01-13 11:59:18 +0530

    file name changed

---
+++ .history/src/app/call-log-form/call-log-form.component_20190113005303.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logform',
+  templateUrl: './call-log-form.component.html',
+  styleUrls: ['./call-log-form.component.css']
+})
+export class CallLogFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
