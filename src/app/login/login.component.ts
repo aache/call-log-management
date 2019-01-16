f46c695063cc130459655cdacc1e59f7d5242172
Naufil865 <naufilmomincs@gmail.com>
2019-01-16 14:39:48 +0530

    dont take backup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginAuthService } from '../services/login/login-auth.service';
+import { AppSettings } from '../app-settings';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginAuthService]
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   submitted: boolean;
   constructor(
     private formBuilder: FormBuilder,
-    private loginAuthService: LoginAuthService
+    private loginAuthService:LoginAuthService
   ) {}
 
   ngOnInit() {
@@ -23,8 +25,9 @@ export class LoginComponent implements OnInit {
   }
 
   get f() { return this.loginForm.controls; }
+  
 
-  onSubmit() {
+  validate() {
     this.submitted = true ;
 
     // stop here if form is invalid
@@ -32,5 +35,11 @@ export class LoginComponent implements OnInit {
         return;
       }
       this.loginAuthService.printLogin(this.f.username.value, this.f.password.value);
+      if(this.loginAuthService.loginAuth(this.f.username.value,this.f.password.value))
+      {
+        window.location.href = AppSettings.PROTOCOL+'://'+AppSettings.HOST+':'+AppSettings.PORT+'/dashboard';
+      }else{
+        alert("FAILURE");
+      }
   }
 }
--- src/app/services/login/login-auth.service.ts
+++ src/app/services/login/login-auth.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AppSettings } from '../../app-settings';
-
+import { Student } from 'src/app/models/Student';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginAuthService {
 
   constructor() { }
-  printLogin(username: String, password: String) {
-    alert(username + '::::' + password + AppSettings.API_ENDPOINT);
-  }
+  loginAuth(u:String , p : String) : boolean
+  {
+    if(u == "roy" && p == "12345")
+    {
+      return true ; 
+    }else {
+      return false ; 
+    }
+}
+printLogin(username: String, password: String) {
+  alert(username + '::::' + password + AppSettings.API_ENDPOINT);
+  console.log(username);    
+
+}
 }
