71b4a5a1ceb6cfd6b62e9aa42c681267b881674a
aaqibhamdule@yahoo.com <aaqibhamdule@yahoo.com>
2019-03-23 19:32:06 +0530

    latest code

---
+++ .history/src/app/models/Outwardpost_20190323193122.ts
@@ -0,0 +1,10 @@
+export class Outwardpost {
+    constructor(
+      public   stock_id : number ,
+      public   username : string,
+      public   quantity : number,
+      public   transition_type : string,
+      public   date : string,
+      public  discription : string
+    ){}
+}
